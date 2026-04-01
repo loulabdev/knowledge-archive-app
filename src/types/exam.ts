e4aa2e2ed60e58b88c07753a26ba6241cf06b417
loulabdev <loulab.dev@gmail.com>
2026-04-01 14:10:19 +0900

    Update exam.ts

--- src/types/exam.ts
+++ src/types/exam.ts
@@ -1,1 +1,17 @@
+export const EXAM_TYPES = ['jeongcheogi', 'sqld'] as const;
+export type ExamType = (typeof EXAM_TYPES)[number];
 
+export const CONTENT_CATEGORIES = ['theory', 'past', 'ox', 'wrong'] as const;
+export type ContentCategory = (typeof CONTENT_CATEGORIES)[number];
+
+export const SUBJECT_TYPES = [
+  'softwareDesign',
+  'softwareDevelopment',
+  'database',
+  'programmingLanguage',
+  'informationSystem',
+  'sqld1',
+  'sqld2',
+] as const;
+
+export type SubjectType = (typeof SUBJECT_TYPES)[number];
